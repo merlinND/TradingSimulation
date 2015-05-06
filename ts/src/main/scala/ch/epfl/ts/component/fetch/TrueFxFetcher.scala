dd0ab570fd756b1d6125797057b05cd49c622828
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-05-06 15:33:28 +0200

    `TrueFXFetcher`: allow user to select currency pairs of interest

--- ts/src/main/scala/ch/epfl/ts/component/fetch/TrueFxFetcher.scala
+++ ts/src/main/scala/ch/epfl/ts/component/fetch/TrueFxFetcher.scala
@@ -7,9 +7,14 @@ import ch.epfl.ts.data.OHLC
 
 /**
  * Fetcher for the TrueFX HTTP API, which provides live Forex quotes for free
+ * 
+ * @param symbols [optional] List of the symbols to fetch. Leave empty to get all available symbols.
+ * 
  * @see TrueFX dev documentation: http://www.truefx.com/dev/data/TrueFX_MarketDataWebAPI_DeveloperGuide.pdf
  */
-class TrueFxFetcher extends PullFetch[Quote]{
+class TrueFxFetcher(symbols: List[(Currency.Currency, Currency.Currency)] = List())
+    extends PullFetch[Quote] {
+  
   val serverBase = "http://webrates.truefx.com/rates/connect.html" + "?f=csv"
 
   val marketId = MarketNames.FOREX_ID
@@ -21,26 +26,23 @@ class TrueFxFetcher extends PullFetch[Quote]{
     for {
       line <- csv.split('\n').toList
       if line.length() > 1 // Eliminate the last empty line
-    } yield {
-      
-  	  val fields = line.split(',')
-		  val currencies = fields(0).split('/').map(s => Currency.fromString(s.toLowerCase))
-		  val timestamp = fields(1).toLong
-		  val values = fields.drop(1).map(s => s.toDouble)
+
+      fields = line.split(',')
+      currencies = fields(0).split('/').map(s => Currency.fromString(s.toLowerCase))
+      timestamp = fields(1).toLong
+      values = fields.drop(1).map(s => s.toDouble)
       
       /**
        * Prices are separated in "big figure" and "points".
        * We can simply concatenate them to obtain the full price.
        */
-      val bid = (fields(2) + fields(3)).toDouble
-      val ask = (fields(4) + fields(5)).toDouble
+      bid = (fields(2) + fields(3)).toDouble
+      ask = (fields(4) + fields(5)).toDouble
       
-    	Quote(
-         marketId, timestamp,
-         currencies(0), currencies(1),
-         bid, ask
-      )
-    }
+      // Filter on the currencies that the user is interested in
+      if( symbols.isEmpty || symbols.contains((currencies(0), currencies(1))) )
+        
+    } yield Quote(marketId, timestamp, currencies(0), currencies(1), bid, ask)
   }
   
   def interval(): Int = 5000
