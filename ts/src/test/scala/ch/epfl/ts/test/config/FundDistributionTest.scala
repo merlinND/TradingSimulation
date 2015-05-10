4d3150c1042b45a8c3fc7ae6283c25f762a0a709
Jakob Ehrl <jakobehrl@gmail.com>
2015-05-10 18:35:05 +0200

    added some more tests for FundDistribution

--- ts/src/test/scala/ch/epfl/ts/test/config/FundDistributionTest.scala
+++ ts/src/test/scala/ch/epfl/ts/test/config/FundDistributionTest.scala
@@ -18,20 +18,67 @@ class FundDistributionTest extends FunSuite {
 //    val fd_AUD = new FundsAustralia
 //    val fd_CAD = new FundsCanada
     
-    val distrInPercent = List(
+    // 1
+    var distrInPercent = List(
+        (1, 50.0),
+        (2, 50.0))
+    var maxVal = distrInPercent.maxBy(_._1)
+    var distr = fd_USD.percentListToDistribution(distrInPercent)
+    
+    var num1s = distr.foldRight(0)((elem, acc) => if (elem == 1) acc + 1 else acc)
+    var num2s = distr.foldRight(0)((elem, acc) => if (elem >= 2) acc + 1 else acc)
+    
+    assert(num1s == 1)
+    assert(num2s == 1)
+    
+    
+    // 2
+    distrInPercent = List(
         (1, 20.0),
         (2, 20.0),
-        (3, 60.0)) // 50 % of the people own more than 2 
-    val maxVal = distrInPercent.maxBy(_._1)
-    val distr = fd_USD.percentListToDistribution(distrInPercent)
+        (3, 60.0)) // 60 % of the people own more than 2 
+    maxVal = distrInPercent.maxBy(_._1)
+    distr = fd_USD.percentListToDistribution(distrInPercent)
     
-    val num1s = distr.foldRight(0)((elem, acc) => if (elem == 1) acc + 1 else acc)
-    val num2s = distr.foldRight(0)((elem, acc) => if (elem == 2) acc + 1 else acc)
-    val num3s = distr.foldRight(0)((elem, acc) => if (elem >= 3) acc + 1 else acc)
+    num1s = distr.foldRight(0)((elem, acc) => if (elem == 1) acc + 1 else acc)
+    num2s = distr.foldRight(0)((elem, acc) => if (elem == 2) acc + 1 else acc)
+    var num3s = distr.foldRight(0)((elem, acc) => if (elem >= 3) acc + 1 else acc)
     
     assert(num1s == 1)
     assert(num2s == 1)
     assert(num3s == 3)
+    
+    // 3
+    distrInPercent = List(
+        (1, 1.0),
+        (2, 95.0),
+        (3, 4.0)) // 4 % of the people own more than 2 
+    maxVal = distrInPercent.maxBy(_._1)
+    distr = fd_USD.percentListToDistribution(distrInPercent)
+    
+    num1s = distr.foldRight(0)((elem, acc) => if (elem == 1) acc + 1 else acc)
+    num2s = distr.foldRight(0)((elem, acc) => if (elem == 2) acc + 1 else acc)
+    num3s = distr.foldRight(0)((elem, acc) => if (elem >= 3) acc + 1 else acc)
+    
+    assert(num1s == 1)
+    assert(num2s == 95)
+    assert(num3s == 4)
+    
+    // 4
+    distrInPercent = List(
+        (1, 3.0),
+        (2, 93.0),
+        (3, 4.0)) // 60 % of the people own more than 2 
+    maxVal = distrInPercent.maxBy(_._1)
+    distr = fd_USD.percentListToDistribution(distrInPercent)
+    
+    num1s = distr.foldRight(0)((elem, acc) => if (elem == 1) acc + 1 else acc)
+    num2s = distr.foldRight(0)((elem, acc) => if (elem == 2) acc + 1 else acc)
+    num3s = distr.foldRight(0)((elem, acc) => if (elem >= 3) acc + 1 else acc)
+    
+    assert(num1s == 1)
+    assert(num2s == 31)
+    assert(num3s == 1)
   }  
   
 }
