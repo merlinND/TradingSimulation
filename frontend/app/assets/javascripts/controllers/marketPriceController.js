8263faa1481a9da4a6215b362a0f1e6c4598a225
Elias Schegg <eliasschegg@gmail.com>
2015-05-10 11:08:05 +0200

    Refactoring: one file per controller

---
+++ frontend/app/assets/javascripts/controllers/marketPriceController.js
@@ -0,0 +1,84 @@
+(function() {
+  'use strict';
+
+  angular.module('myApp').controller('MarketPriceController', [ '$scope', 'alertService',
+      function($scope, alertService) {
+        $scope.alerts = alertService.get();
+
+        var ws = new WebSocket('ws://localhost:9000/fetchers/quote');
+
+        ws.onmessage = function(event) {
+          var quote = JSON.parse(event.data);
+          $scope.$apply(function() {
+            var name = quote.whatC.name + " to " + quote.withC.name;
+
+            var quoteSeries = $scope.chartSeries.filter(function(series) {
+              return series.name == name;
+            })[0];
+
+            if (!quoteSeries) {
+              quoteSeries = {
+                'name' : name,
+                'visible' : false,
+                'data' : []
+              };
+
+              if ($scope.chartSeries.length === 0) {
+                quoteSeries.visible = true;
+              }
+
+              $scope.chartSeries.push(quoteSeries);
+            }
+
+            quoteSeries.data.push([ quote.timestamp, quote.ask ]);
+
+            if ($scope.chartConfig.loading) {
+              $scope.chartConfig.loading = false;
+            }
+
+          });
+        };
+
+        ws.onclose = function(event) {
+          $scope.$apply(function() {
+            alertService.add('info', 'Closed connection to the backend');
+          });
+        };
+
+        ws.onerror = function(event) {
+          $scope.$apply(function() {
+            alertService.add('danger', 'Lost connection to the backend');
+          });
+        };
+
+        $scope.chartSeries = [];
+
+        $scope.chartConfig = {
+          options : {
+            xAxis : [ {
+              type : 'datetime'
+            } ],
+            navigator : {
+              enabled : true,
+              series : {
+                data : $scope.chartSeries[0]
+              }
+            },
+            rangeSelector : {
+              enabled : true
+            },
+          },
+          series : $scope.chartSeries,
+          title : {
+            text : 'Market price'
+          },
+          credits : {
+            enabled : false
+          },
+          loading : true,
+          size : {}
+        };
+
+      } ]);
+
+})();
