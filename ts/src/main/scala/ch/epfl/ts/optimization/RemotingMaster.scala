0a389667291bf94973a5967c4e349e0634adeabd
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-05-12 17:11:09 +0200

    Refactor remoting experiments into a nice generic class (draft)

--- ts/src/main/scala/ch/epfl/ts/optimization/RemotingMaster.scala
+++ ts/src/main/scala/ch/epfl/ts/optimization/RemotingMaster.scala
@@ -57,28 +57,6 @@ class MasterActor extends Actor {
 }
 
 /**
- * Dummy actor to test Akka remoting
- */
-class WorkerActor(hostActor: ActorRef, dummyParam: Int) extends Actor {
-
-  println("Worker actor with param " + dummyParam + " now running.")
-  hostActor ! WorkerIsLive
-
-  override def receive = {
-    case s: String => {
-      println("WorkerActor received: " + s)
-      hostActor ! "Hi there!"
-    }
-    case m => println("WorkerActor received weird: " + m)
-  }
-}
-
-/* Need this concrete class to help serialization */
-class QuoteTag extends ClassTag[Quote] with Serializable {
-  override def runtimeClass = classOf[Quote]
-}
-
-/**
  * Runs a main() method that creates a MasterActor and remote WorkerActors
  * by calling createRemoteActors() for every availableWorker. It assumes
  * that there is a RemotingWorker class running and listening on port 3333
@@ -100,76 +78,6 @@ object RemotingHost {
   // TODO: lookup in configuration
   val workerSystemName = "remote"
 
-  /**
-   * Define a list of props (actor types) that will be created identically on
-   * all actor systems (i.e. every worker will run these actors):
-   */
-  val commonProps = {
-    // Fetcher
-    val fetcher = new TrueFxFetcher
-    val fetcherActor = Props(classOf[PullFetchComponent[Quote]], fetcher, new QuoteTag)
-
-    // Market
-    val rules = new ForexMarketRules()
-    val market = Props(classOf[MarketFXSimulator], MarketNames.FOREX_ID, rules)
-
-    // Printer
-    val printer = Props(classOf[Printer], "")
-
-    Map(
-      "fetcher" -> fetcherActor,
-      "market"  -> market,
-      "printer" -> printer
-    )
-  }
-
-  /**
-   * For the given remote worker, create the common components
-   * and an instance of the trading strategy for each parameter value given.
-   *
-   * @param master            Supervisor actor to register the worker to
-   * @param host              Hostname of the remote actor system
-   * @param prefix            Prefix to use in each of this system's actor names
-   * @param parameterValues
-   */
-  // TODO: replace by actual parameters
-  // TODO: do not hardcode strategy
-  // TODO: connect actors
-  def createRemoteActors(master: ComponentRef, host: String, prefix: String,
-                         parameterValues: Iterable[Int])
-                        (implicit builder: ComponentBuilder): Unit = {
-    val address = Address("akka.tcp", workerSystemName, host, workerPort)
-    val deploy = Deploy(scope = RemoteScope(address))
-
-    // Common props
-    val common = commonProps.map({ case (name, props) =>
-      println("Creating common prop " + (prefix + "-" + name) + " at host " + host)
-      (name -> builder.createRef(props.withDeploy(deploy), prefix + name))
-    })
-
-    // One trader for each parameterization
-    for {
-      dummyParam <- parameterValues
-    } yield {
-      val name = prefix +  "-Worker-" + dummyParam
-
-      // TODO: evaluator as well
-      val trader = builder.createRef(Props(classOf[WorkerActor], master.ar, dummyParam).withDeploy(deploy), name)
-
-      // Register this new trader to the master
-      master.ar ! trader.ar
-      // Connect this new trader to the required components
-      // TODO: support all order types
-      trader -> (common("market"), classOf[MarketAskOrder], classOf[MarketBidOrder])
-
-      println("Created trader " + name + " at host " + host)
-    }
-
-    // TODO: all useful connections
-    common("fetcher") -> (common("printer"), classOf[Quote])
-    common("market")  -> (common("printer"), classOf[Transaction])
-  }
-
   def main(args: Array[String]): Unit = {
 
     // `akka.remote.netty.tcp.hostname` is specified on a per-machine basis in the `application.conf` file
@@ -181,23 +89,15 @@ akka.remote.netty.tcp.port = 3333
 akka.actor.serialize-creators = on
 """).withFallback(ConfigFactory.load());
 
-    // Build the master actor
+    // Build the supervisor actor
     implicit val builder = new ComponentBuilder("host", remotingConfig)
     val master = builder.createRef(Props(classOf[MasterActor]), "MasterActor")
 
-    // Prepare parameters for worker actors (there will be one actor per parameter value)
-    val allParameterValues = (1 to 10)
-    val slicedParameters = {
-      val n = (allParameterValues.length / availableWorkers.length.toFloat).ceil.toInt
-      (0 until availableWorkers.length).map(i => allParameterValues.slice(n * i, (n * i) + n))
-    }
-
-    // Build remote worker actors
-    val remoteActors = for {
-      (workerHost, idx) <- availableWorkers.zipWithIndex
-    } yield createRemoteActors(master, workerHost, idx.toString(), slicedParameters(idx))
+    // TODO: generate parameterizations
+    // TODO: create actors on each host
 
     builder.start
     // TODO: handle evaluator reports on stop
+    // TODO: select the best strategy
   }
 }
---
+++ ts/src/main/scala/ch/epfl/ts/optimization/StrategyFactory.scala
@@ -0,0 +1,174 @@
+package ch.epfl.ts.optimization
+
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
+import akka.actor.Deploy
+import ch.epfl.ts.engine.MarketFXSimulator
+import akka.actor.Props
+import ch.epfl.ts.component.ComponentBuilder
+import ch.epfl.ts.engine.ForexMarketRules
+import ch.epfl.ts.component.fetch.TrueFxFetcher
+import ch.epfl.ts.component.ComponentRef
+import ch.epfl.ts.component.fetch.MarketNames
+import ch.epfl.ts.component.fetch.PullFetchComponent
+import akka.actor.Address
+import ch.epfl.ts.component.utils.Printer
+import ch.epfl.ts.data.Quote
+import akka.remote.RemoteScope
+import scala.reflect.ClassTag
+import ch.epfl.ts.data.StrategyParameters
+import ch.epfl.ts.traders.TraderCompanion
+import ch.epfl.ts.brokers.StandardBroker
+import ch.epfl.ts.data.Currency
+import ch.epfl.ts.evaluation.Evaluator
+
+
+trait StrategyFactory {
+  
+  /* Need this concrete class to help serialization */
+  class QuoteTag extends ClassTag[Quote] with Serializable {
+    override def runtimeClass = classOf[Quote]
+  }
+  
+  abstract class CommonProps {
+    // Required components for a system to work
+    def fetcher: Props
+    def market: Props
+    def broker: Props
+    
+    def marketIds: List[Long]
+    
+    // Optional components
+    def printer: Option[Props] = None
+  }
+  
+  /**
+   * @param namingPrefix Prefix that will precede the name of each actor on this remote
+   * @param systemName Name of the actor system being run on this remote
+   */
+  class RemoteHost(val hostname: String, val port: Int, val namingPrefix: String, val systemName: String = "remote") {
+    val address = Address("akka.tcp", systemName, hostname, port)
+    val deploy = Deploy(scope = RemoteScope(address))
+    
+    def createRemotely(props: Props, name: String)(implicit builder: ComponentBuilder): ComponentRef = {
+      // TODO: use `log.debug`
+      val actualName = namingPrefix + '-' + name
+		  println("Creating remotely component " + actualName + " at host " + hostname)
+    	builder.createRef(props.withDeploy(deploy), actualName)
+    }
+  }
+  
+  
+  /**
+   * Define a set of props (used to create components) that will be created identically on
+   * all actor systems (i.e. every worker will run these actors):
+   */
+  def commonProps: CommonProps
+  
+  
+  
+  /**
+   * For the given remote worker, create the common components
+   * and an instance of the trading strategy for each parameterization given.
+   *
+   * @param master            Supervisor actor to register the remote actors to
+   * @param host              Remote machine on which to deploy the new actors
+   * @param parameterizations
+   * 
+   * @return A list of references to all the instances of the strategy being optimized
+   */
+  // TODO: connect components smartly
+  def createRemoteActors(master: ComponentRef, host: RemoteHost,
+                         strategyToOptimize: TraderCompanion, parameterizations: Set[StrategyParameters])
+                        (implicit builder: ComponentBuilder): List[ComponentRef]
+  
+
+  /**
+   * @return A list of the Evaluator components references (one for each parameterization passed)
+   *         These must receive Transactions from the market in order to evaluate.
+   *         Also, it should be used just as a reference to the Trader itself since it will forward
+   *         the messages.
+   */
+  protected def createRemoteTraders(host: RemoteHost, strategyToOptimize: TraderCompanion,
+                                  parameterizations: Set[StrategyParameters])
+                                 (implicit builder: ComponentBuilder): List[ComponentRef] = {
+    // One trader for each parameterization
+    for((parameterization, i) <- parameterizations.toList.zipWithIndex) yield {
+      
+      // Assert that parameterization is valid for this strategy (will throw if it is not the case)
+      strategyToOptimize.verifyParameters(parameterization)
+      
+      // TODO: user of this class could give a list of names
+      val name = host.hostname +  "-Trader-" + i
+
+      // Trader
+      val traderId = i
+      val traderProps = Props(strategyToOptimize.ConcreteTrader, traderId, commonProps.marketIds, parameterization)
+      val trader = host.createRemotely(traderProps, name)
+      // Evaluator monitoring the performance of this trader
+      // TODO: factor these out
+      val period = 10 seconds
+      val referenceCurrency = Currency.CHF
+      val evaluator = builder.createRef(Props(classOf[Evaluator], trader, traderId, referenceCurrency, period), name + "-Evaluator")
+    
+      evaluator
+    }
+  }
+}
+
+
+/**
+ * Concrete StrategyFactory allowing to test Forex traders.
+ */
+object ForexStrategyFactory extends StrategyFactory {
+
+  
+  def commonProps = new CommonProps {
+    // Fetcher
+    def fetcherActor = {
+      val fetcher = new TrueFxFetcher
+      Props(classOf[PullFetchComponent[Quote]], fetcher, new QuoteTag)
+    }
+    
+    def marketIds = List(MarketNames.FOREX_ID)
+
+    // Market
+    def market = {
+      val rules = new ForexMarketRules()
+      Props(classOf[MarketFXSimulator], marketIds(0), rules)
+    }
+    
+    def broker = {
+      Props(classOf[StandardBroker])
+    }
+    
+    // Printer
+    override def printer = Some(Props(classOf[Printer], "MyPrinter"))
+  }
+  
+  
+  def createRemoteActors(master: ComponentRef, host: RemoteHost,
+                         strategyToOptimize: TraderCompanion, parameterizations: Set[StrategyParameters])
+                        (implicit builder: ComponentBuilder): List[ComponentRef] = {
+
+    // ----- Common props (need one instance per host, used by all the traders)
+    val fetcher = host.createRemotely(commonProps.fetcher, "Fetcher")
+		val market = host.createRemotely(commonProps.market, "Market")
+		val broker = host.createRemotely(commonProps.broker, "Broker")
+		val printer = commonProps.printer.map(p => host.createRemotely(p, "Printer"))
+
+    // ----- Traders (possibly many) to be run in parallel on this host
+    val evaluators = createRemoteTraders(host, strategyToOptimize, parameterizations)
+
+    
+    // ----- Connections
+    // TODO
+    
+    // ----- Registration to the supervisor
+    // Register this new trader to the master
+    for(e <- evaluators) master.ar ! e.ar
+    
+    evaluators
+  }
+  
+}
