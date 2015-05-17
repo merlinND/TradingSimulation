97a05fc1334198a856a30b603b161863ea6b5acd
Jakob Ehrl <jakobehrl@gmail.com>
2015-05-17 16:48:38 +0200

    almost restored expected behavior (market maker is not receiving messages yet when order books empty)

--- ts/src/main/scala/ch/epfl/ts/engine/HybridMarketSimulator.scala
+++ ts/src/main/scala/ch/epfl/ts/engine/HybridMarketSimulator.scala
@@ -8,6 +8,7 @@ import akka.actor.ActorLogging
 import ch.epfl.ts.component.utils.Timekeeper
 import akka.actor.Props
 import ch.epfl.ts.data.TheTimeIs
+import scala.slick.direct.order
 
 /**
  * Market simulator, where first the data is being received from fetcher and market behaves in a way that traders' orders
@@ -78,15 +79,16 @@ class HybridMarketSimulator(marketId: Long, rules1: FxMarketRulesWrapper, rules2
   def playMarketMaker() = {
     val asksEmpty = if (book.asks.isEmpty) 1 else 0
     val bidsEmpty = if (book.bids.isEmpty) 1 else 0
-    if (asksEmpty + bidsEmpty == 1){
-      val order = if (asksEmpty == 1){
+    if (asksEmpty + bidsEmpty >= 1){
+      val msg = if (asksEmpty == 1){
         val topBid = book.bids.head
-        LimitAskOrder(1, -1, topBid.timestamp, topBid.whatC, topBid.withC, topBid.volume, topBid.price * (1 + spread))
-      } else {
+        MarketAsksEmpty(topBid.timestamp, topBid.whatC, topBid.withC, topBid.volume, topBid.price)
+      } else if (bidsEmpty == 1) {
         val topAsk = book.asks.head
-        LimitBidOrder(1, -1, topAsk.timestamp, topAsk.whatC, topAsk.withC, topAsk.volume, topAsk.price * (1 - spread))
+        MarketBidsEmpty(topAsk.timestamp, topAsk.whatC, topAsk.withC, topAsk.volume, topAsk.price)
+      } else {
+        log.info("No orders at all")
       }
-      receiver(order)
     }
   }
 
